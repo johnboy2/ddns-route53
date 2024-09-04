500a78c11f0ce3d20a5c534c23c27d882d1a396d
John Nelson <john@jnelson.ca>
2024-09-03 23:58:00 -0600

    Get algo indirection to compile

--- src/config.rs
+++ src/config.rs
@@ -1,15 +1,20 @@
 use core::str;
 use std::fs::File;
+use std::future::Future;
 use std::io::BufReader;
 use std::io::Read;
 use std::io::Seek;
+use std::net::{Ipv4Addr, Ipv6Addr};
+use std::ops::FnOnce;
+use std::pin::Pin;
 use std::time::Duration;
 use std::vec::Vec;
 
+use derivative::Derivative;
 use serde::Deserialize;
 
 
-static DEFAULT_ALGO_TIMEOUT: f64 = 10.0;
+static DEFAULT_ALGO_TIMEOUT_SECONDS: f64 = 10.0;
 fn default_update_poll_seconds() -> f64 { return 30.0; }
 fn default_update_timeout_seconds() -> f64 { return 300.0; }
 static MAX_CONFIG_FILE_SIZE: u64 = 65536;
@@ -19,22 +24,12 @@ static MAX_UPDATE_TIMEOUT_SECONDS: f64 = 3600.0;
 
 #[derive(Deserialize)]
 #[serde(tag = "type")]
-enum V4algo {
+enum AlgorithmSpecification {
     #[serde(rename = "default_public_ip")]
     DefaultPublicIp,
 
     #[serde(rename = "internet_gateway_protocol")]
     InternetGatewayProtocol {timeout_seconds: Option::<f64>},
-
-    #[serde(rename = "web_service")]
-    WebService {url: String, timeout_seconds: Option::<f64>}
-}
-
-#[derive(Deserialize)]
-#[serde(tag = "type")]
-enum V6algo {
-    #[serde(rename = "default_public_ip")]
-    DefaultPublicIp,
 
     #[serde(rename = "web_service")]
     WebService {url: String, timeout_seconds: Option::<f64>}
@@ -51,8 +46,8 @@ struct FileConfig {
     #[serde(default = "default_update_timeout_seconds")]
     update_timeout_seconds: f64,
 
-    ipv4_algorithms: Vec::<V4algo>,
-    ipv6_algorithms: Vec::<V6algo>,
+    ipv4_algorithms: Vec::<AlgorithmSpecification>,
+    ipv6_algorithms: Vec::<AlgorithmSpecification>,
     aws_profile: Option<String>,
     aws_secret_key: Option<String>,
     aws_secret_access_key: Option<String>,
@@ -128,14 +123,172 @@ fn read_config_file(config_path: &String) -> Result<FileConfig, String> {
 }
 
 
-#[derive(Debug)]
+type V4AlgoFn = dyn FnOnce() -> Pin<Box<dyn Future<Output = Result::<Vec::<Ipv4Addr>, String>>>>;
+type V6AlgoFn = dyn FnOnce() -> Pin<Box<dyn Future<Output = Result::<Vec::<Ipv6Addr>, String>>>>;
+
+
+#[derive(Derivative)]
+#[derivative(Debug)]
 pub struct Config {
     host_name: String,
     update_poll_interval: Duration,
     update_timeout: Duration,
 
+    #[derivative(Debug="ignore")]
+    ipv4_algo_fns: Vec::<Box<V4AlgoFn>>,
+    ipv4_algorithms: Vec::<String>,
+
+    #[derivative(Debug="ignore")]
+    ipv6_algo_fns: Vec::<Box<V6AlgoFn>>,
+    ipv6_algorithms: Vec::<String>,
+
     route53_zone_id: Option::<String>,
 }
+
+
+struct V4AlgoResult {
+    descriptions: Vec::<String>,
+    functions: Vec::<Box<V4AlgoFn>>,
+}
+
+
+fn build_v4_algos(specs: &Vec::<AlgorithmSpecification>) -> Result<V4AlgoResult, String> {
+    let mut have_default = false;
+    let mut have_igd = false;
+    let mut have_web_service_url = std::collections::HashSet::<&str>::with_capacity(specs.len());
+    let mut descriptions = Vec::<String>::with_capacity(specs.len());
+    let mut functions = Vec::<Box<V4AlgoFn>>::new();
+    for spec in specs.iter() {
+        match spec {
+            AlgorithmSpecification::DefaultPublicIp => {
+                if have_default {
+                    return Err("config:ipv4_algorithms can only have up to one default_public_ip".to_owned());
+                }
+                have_default = true;
+                descriptions.push(String::from("{type=default_public_ip}"));
+                functions.push(
+                    Box::new(
+                        || Box::pin(crate::ip_algorithms::get_default_public_ip_v4())
+                    )
+                );
+            },
+            AlgorithmSpecification::InternetGatewayProtocol { timeout_seconds } => {
+                if have_igd {
+                    return Err("config:ipv4_algorithms can only have up to one internet_gateway_protocol".to_owned());
+                }
+                have_igd = true;
+                let mut description = String::from("{type=internet_gateway_protocol");
+                let timeout = match timeout_seconds {
+                    Some(timeout_secs) => {
+                        description += format!(", timeout_seconds={timeout_secs}").as_str();
+                        process_timeout(*timeout_secs, None)?
+                    },
+                    None => Duration::from_secs_f64(DEFAULT_ALGO_TIMEOUT_SECONDS),
+                };
+                description += "}";
+                descriptions.push(description);
+                functions.push(
+                    Box::new(
+                        move || Box::pin(crate::ip_algorithms::get_igd_ip_v4(timeout))
+                    )
+                );
+            },
+            AlgorithmSpecification::WebService { url, timeout_seconds } => {
+                if !have_web_service_url.insert(url.as_str()) {
+                    return Err(format!("config:ipv4_algorithms can only have up to one web_service:[{}]", url));
+                }
+                let mut description = format!("{{type=web_service, url=\"{url}\"");
+                let url_parsed = match reqwest::Url::parse(url) {
+                    Ok(parsed) => parsed,
+                    Err(e) => {
+                        return Err(format!("Failed to parse URL [{}]: {}", url, e.to_string()))
+                    }
+                };
+                let url_owned = url.to_owned();
+                let timeout = match timeout_seconds {
+                    Some(timeout_secs) => {
+                        description += format!(", timeout_seconds={timeout_secs}").as_str();
+                        process_timeout(*timeout_secs, None)?
+                    },
+                    None => Duration::from_secs_f64(DEFAULT_ALGO_TIMEOUT_SECONDS),
+                };
+                description += "}";
+                descriptions.push(description);
+                functions.push(
+                    Box::new(
+                        move || Box::pin(crate::ip_algorithms::get_web_service_ip_v4(url_parsed, url_owned, timeout))
+                    )
+                )
+            },
+        };
+    }
+
+    Ok(V4AlgoResult {descriptions, functions})
+}
+
+
+struct V6AlgoResult {
+    descriptions: Vec::<String>,
+    functions: Vec::<Box<V6AlgoFn>>,
+}
+
+
+fn build_v6_algos(specs: &Vec::<AlgorithmSpecification>) -> Result<V6AlgoResult, String> {
+    let mut have_default = false;
+    let mut have_web_service_url = std::collections::HashSet::<&str>::with_capacity(specs.len());
+    let mut descriptions = Vec::<String>::with_capacity(specs.len());
+    let mut functions = Vec::<Box<V6AlgoFn>>::new();
+    for spec in specs.iter() {
+        match spec {
+            AlgorithmSpecification::DefaultPublicIp => {
+                if have_default {
+                    return Err("config:ipv6_algorithms can only have up to one default_public_ip".to_owned());
+                }
+                have_default = true;
+                descriptions.push(String::from("{type=default_public_ip}"));
+                functions.push(
+                    Box::new(
+                        || Box::pin(crate::ip_algorithms::get_default_public_ip_v6())
+                    )
+                );
+            },
+            AlgorithmSpecification::InternetGatewayProtocol { timeout_seconds: _ } => {
+                // TODO: consider making this error the same as any other unknown/invalid "type"
+                return Err("config:ipv6_algorithms cannot have internet_gateway_protocol".to_owned());
+            },
+            AlgorithmSpecification::WebService { url, timeout_seconds } => {
+                if !have_web_service_url.insert(url.as_str()) {
+                    return Err(format!("config:ipv6_algorithms can only have up to one web_service:[{}]", url));
+                }
+                let mut description = format!("{{type=web_service, url=\"{url}\"");
+                let url_parsed = match reqwest::Url::parse(url) {
+                    Ok(parsed) => parsed,
+                    Err(e) => {
+                        return Err(format!("Failed to parse URL [{}]: {}", url, e.to_string()))
+                    }
+                };
+                let url_owned = url.to_owned();
+                let timeout = match timeout_seconds {
+                    Some(timeout_secs) => {
+                        description += format!(", timeout_seconds={timeout_secs}").as_str();
+                        process_timeout(*timeout_secs, None)?
+                    },
+                    None => Duration::from_secs_f64(DEFAULT_ALGO_TIMEOUT_SECONDS),
+                };
+                description += "}";
+                descriptions.push(description);
+                functions.push(
+                    Box::new(
+                        move || Box::pin(crate::ip_algorithms::get_web_service_ip_v6(url_parsed, url_owned, timeout))
+                    )
+                )
+            },
+        };
+    }
+
+    Ok(V6AlgoResult {descriptions, functions})
+}
+
 
 impl Config {
     pub fn load(config_path: &String) -> Result<Self, String> {
@@ -149,12 +302,19 @@ impl Config {
             config_file.update_timeout_seconds, 
             Some(MAX_UPDATE_TIMEOUT_SECONDS)
         )?;
+        let v4_algos = build_v4_algos(&config_file.ipv4_algorithms)?;
+        let v6_algos = build_v6_algos(&config_file.ipv6_algorithms)?;
 
         Ok(Self {
             host_name: config_file.host_name,
             update_poll_interval: poll_interval,
             update_timeout: timeout,
+            ipv4_algorithms: v4_algos.descriptions,
+            ipv4_algo_fns: v4_algos.functions,
+            ipv6_algorithms: v6_algos.descriptions,
+            ipv6_algo_fns: v6_algos.functions,
             route53_zone_id: config_file.aws_route53_zone_id,
         })
     }
 }
+ 
--- src/ip_algorithms.rs
+++ src/ip_algorithms.rs
@@ -2,6 +2,7 @@ use std::net::{Ipv4Addr, Ipv6Addr};
 use std::str::FromStr;
 use std::string::ToString;
 use std::sync::LazyLock;
+use std::time::Duration;
 use std::vec::Vec;
 
 use netdev::Interface;
@@ -10,6 +11,17 @@ use netdev::Interface;
 static DEFAULT_INTERFACE: LazyLock<Result<Interface, String>> = LazyLock::new(||
     netdev::get_default_interface()
 );
+
+
+static WEB_CLIENT: LazyLock<Result<reqwest::Client, String>> = LazyLock::new(|| {
+    match reqwest::ClientBuilder::new().build() {
+        Ok(client) => Ok(client),
+        Err(e) => Err(e.to_string())
+    }
+});
+
+
+const MAX_WEB_SERVICE_DOCUMENT_LENGTH: u64 = 65536;
 
 
 // Adapted from https://doc.rust-lang.org/1.80.1/src/core/net/ip_addr.rs.html#763-779
@@ -84,6 +96,55 @@ fn ipv6_is_global(ip: &Ipv6Addr) -> bool {
 }
 
 
+// Helper to download a document from a URL
+async fn get_web_service_document(
+    client: &reqwest::Client, url: reqwest::Url, url_string: &String, timeout: Duration
+) -> Result<String, String> {
+    let request =
+        client.get(url)
+        .timeout(timeout)
+        .send()
+    ;
+
+    let response = match request.await {
+        Ok(r) => r,
+        Err(e) => {
+            return Err(format!(
+                "Failed to fetch from URL \"{}\": {}",
+                url_string,
+                e.to_string()
+            ))
+        }
+    };
+
+    match response.content_length() {
+        Some(cl) => {
+            if MAX_WEB_SERVICE_DOCUMENT_LENGTH < cl {
+                return Err(format!(
+                    "Result body from URL \"{}\": Content-Length ({}) too large (max={})",
+                    url_string, cl, MAX_WEB_SERVICE_DOCUMENT_LENGTH
+                ));
+            }
+        },
+        None => {}
+    };
+
+    // TODO: implement a maximum read size for a streaming response
+
+    let body = match response.text().await {
+        Ok(b) => b,
+        Err(e) => {
+            return Err(format!(
+                "Failed to read result body from URL \"{}\": {}",
+                url_string,
+                e.to_string()
+            ))
+        }
+    };
+    Ok(body)
+}
+
+
 pub async fn get_default_public_ip_v4() -> Result::<Vec::<Ipv4Addr>, String> {
     let default_interface = match &*DEFAULT_INTERFACE {
         Ok(interface) => interface,
@@ -94,6 +155,75 @@ pub async fn get_default_public_ip_v4() -> Result::<Vec::<Ipv4Addr>, String> {
     for ip_net in &default_interface.ipv4 {
         if ipv4_is_global(&ip_net.addr) {
             result.push(ip_net.addr.to_owned())
+        }
+    }
+
+    Ok(result)
+}
+
+
+pub async fn get_igd_ip_v4(timeout: Duration) -> Result<Vec::<Ipv4Addr>, String> {
+    let thread_result: Result<Result<Vec<Ipv4Addr>, String>, tokio::task::JoinError> = tokio::task::spawn_blocking(move || {
+        let search_option = igd_next::SearchOptions {
+            timeout: Some(timeout),
+            ..Default::default()
+        };
+        let gateway = match igd_next::search_gateway(search_option) {
+            Ok(gw) => gw,
+            Err(e) => {
+                return Err(
+                    format!("Failed to find internet gateway: {}", e.to_string())
+                );
+            }
+        };
+
+        match gateway.get_external_ip() {
+            Ok(ip) => {
+                match ip {
+                    std::net::IpAddr::V4(v4) => {
+                        return Ok(vec![v4]);
+                    },
+                    _ => {
+                        // Silently ignore this case
+                        return Ok(Vec::<Ipv4Addr>::new())
+                    }
+                }
+            },
+            Err(e) => {
+                return Err(
+                    format!("Failed to determine internet gateway external IP address: {}", e.to_string())
+                );
+            }
+        }
+    }).await;
+
+    match thread_result {
+        Ok(result) => result,
+        Err(e) => {
+            return Err(format!(
+                "Error joining thread searching for internet gateway device: {}",
+                e.to_string()
+            ))
+        }
+    }
+}
+
+
+pub async fn get_web_service_ip_v4(url: reqwest::Url, url_string: String, timeout: Duration) -> Result::<Vec::<Ipv4Addr>, String> {
+    let client = (&*WEB_CLIENT).as_ref()?;
+
+    let body = get_web_service_document(client, url, &url_string, timeout).await?;
+
+    let mut result = Vec::<Ipv4Addr>::new();
+    for line in body.as_str().lines() {
+        match Ipv4Addr::from_str(line) {
+            Ok(ip) => { result.push(ip) },
+            Err(e) => {
+                return Err(format!(
+                    "Failed to parse result as IPv4 address: \"{}\": {}",
+                    line, e.to_string().as_str()
+                ))
+            }
         }
     }
 
@@ -118,136 +248,23 @@ pub async fn get_default_public_ip_v6() -> Result::<Vec::<Ipv6Addr>, String> {
 }
 
 
-struct WebServiceInvocation {
-    uri_string: String,
-    uri_parsed: reqwest::Url,
-    timeout: std::time::Duration,
-    client: reqwest::Client,
-}
+pub async fn get_web_service_ip_v6(url: reqwest::Url, url_string: String, timeout: Duration) -> Result::<Vec::<Ipv6Addr>, String> {
+    let client = (&*WEB_CLIENT).as_ref()?;
 
-trait Ipv4OrIpv6 : FromStr + ToString {}
-impl Ipv4OrIpv6 for Ipv4Addr {}
-impl Ipv4OrIpv6 for Ipv6Addr {}
+    let body = get_web_service_document(client, url, &url_string, timeout).await?;
 
-
-impl WebServiceInvocation {
-    pub fn new(url: &String, timeout_seconds: f64) -> Result::<Self, String> {
-        let uri = match reqwest::Url::parse(url) {
-            Ok(uri) => uri,
-            Err(e) => { return Err(e.to_string()); }
-        };
-        
-        Ok(Self {
-            uri_string: url.to_owned(),
-            uri_parsed: uri,
-            timeout: std::time::Duration::from_secs_f64(timeout_seconds),
-            client: reqwest::Client::new(),
-        })
-    }
-
-    pub async fn get_ip_address<T: Ipv4OrIpv6>(&self) -> Result<Vec::<T>, String>
-    where <T as FromStr>::Err: ToString
-    {
-        let request = self.client.get(self.uri_parsed.to_owned())
-            .timeout(self.timeout)
-            .send()
-        ;
-        let response = match request.await {
-            Ok(r) => r,
+    let mut result = Vec::<Ipv6Addr>::new();
+    for line in body.as_str().lines() {
+        match Ipv6Addr::from_str(line) {
+            Ok(ip) => { result.push(ip) },
             Err(e) => {
                 return Err(format!(
-                    "Failed to fetch from URL \"{}\": {}",
-                    self.uri_string, 
-                    e.to_string()
-                ))
-            }
-        };
-        let body = match response.text().await {
-            Ok(b) => b,
-            Err(e) => {
-                return Err(format!(
-                    "Failed to read result body from URL \"{}\": {}",
-                    self.uri_string, 
-                    e.to_string()
-                ))
-            }
-        };
-
-        let mut result = Vec::<T>::new();
-        for line in body.as_str().lines() {
-            match T::from_str(line) {
-                Ok(ip) => { result.push(ip) },
-                Err(e) => {
-                    return Err(format!(
-                        "Failed to parse result as IPv4 address: \"{}\": {}",
-                        line, e.to_string().as_str()
-                    ))
-                }
-            }
-        }
-
-        Ok(result)
-    }
-}
-
-
-struct IgdSearchInvocation {
-    timeout: std::time::Duration,
-}
-
-
-impl IgdSearchInvocation {
-    pub fn new(timeout_seconds: f64) -> Self {
-        Self {
-            timeout: std::time::Duration::from_secs_f64(timeout_seconds),
-        }
-    }
-
-    pub async fn get_ip_address_v4(&self) -> Result<Vec::<Ipv4Addr>, String> {
-        let timeout = Some(self.timeout);
-
-        let thread_result: Result<Result<Vec<Ipv4Addr>, String>, tokio::task::JoinError> = tokio::task::spawn_blocking(move || {
-            let search_option = igd_next::SearchOptions {
-                timeout: timeout,
-                ..Default::default()
-            };
-            let gateway = match igd_next::search_gateway(search_option) {
-                Ok(gw) => gw,
-                Err(e) => {
-                    return Err(
-                        format!("Failed to find internet gateway: {}", e.to_string())
-                    );
-                }
-            };
-
-            match gateway.get_external_ip() {
-                Ok(ip) => {
-                    match ip {
-                        std::net::IpAddr::V4(v4) => {
-                            return Ok(vec![v4]);
-                        },
-                        _ => {
-                            // Silently ignore this case
-                            return Ok(Vec::<Ipv4Addr>::new())
-                        }
-                    }
-                },
-                Err(e) => {
-                    return Err(
-                        format!("Failed to determine internet gateway external IP address: {}", e.to_string())
-                    );
-                }
-            }
-        }).await;
-
-        match thread_result {
-            Ok(result) => result,
-            Err(e) => {
-                return Err(format!(
-                    "Error joining thread searching for internet gateway device: {}",
-                    e.to_string()
+                    "Failed to parse result as IPv6 address: \"{}\": {}",
+                    line, e.to_string().as_str()
                 ))
             }
         }
     }
+
+    Ok(result)
 }
