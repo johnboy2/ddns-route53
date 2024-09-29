11177afb222a1eedfc8647d8dcd8ec7035c1c80c
John Nelson <john@jnelson.ca>
2024-09-29 16:48:36 -0600

    Allow address comparisons

--- src/addresses.rs
+++ src/addresses.rs
@@ -2,7 +2,7 @@ use std::net::{Ipv4Addr, Ipv6Addr};
 use std::vec::Vec;
 
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct Addresses {
     pub v4: Vec::<Ipv4Addr>,
     pub v6: Vec::<Ipv6Addr>,
