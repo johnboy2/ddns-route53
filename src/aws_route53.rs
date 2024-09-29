24983e190a4ed6f0e4588b115135dfa99ae789b7
John Nelson <john@jnelson.ca>
2024-09-29 16:50:51 -0600

    Add route53 implementation

--- src/aws_route53.rs
+++ src/aws_route53.rs
@@ -1,7 +1,16 @@
+use std::net::{Ipv4Addr, Ipv6Addr};
+use std::time::Instant;
+
 use aws_config::profile::ProfileFileCredentialsProvider;
 use aws_sdk_route53::config::Credentials;
 use aws_sdk_route53::Client;
+use aws_sdk_route53::types::{ResourceRecord, RrType};
 use aws_types::region::Region;
+use futures::join;
+use tokio::time::{sleep, timeout};
+
+use crate::addresses::Addresses;
+use crate::config::Config;
 
 
 pub async fn get_client(
@@ -41,3 +50,286 @@ pub async fn get_client(
     client   
 }
 
+
+fn host_is_in_domain(host_lowercase: &str, domain: &str) -> bool {
+    let domain = domain.to_lowercase();
+
+    if host_lowercase == domain {
+        return true;
+    }
+    if host_lowercase.ends_with(domain.as_str()) {
+        let host = host_lowercase.as_bytes();
+        let domain = domain.as_bytes();
+        let maybe_separator = host[host.len() - domain.len() - 1];
+        if maybe_separator == b'.' {
+            return true;
+        }
+    }
+
+    false
+}
+
+
+pub async fn get_zone_id(client: &Client, host_name: &String) -> Result<String, String> {
+    let mut stream = client.list_hosted_zones().into_paginator().send();
+    while let Some(page) = stream.next().await {
+        match page {
+            Ok(result) => {
+                for zone in result.hosted_zones.iter() {
+                    if host_is_in_domain(host_name.as_str(), zone.name()) {
+                        return Ok(zone.id.to_owned())
+                    }
+                }
+            },
+            Err(e) => {
+                return Err(e.to_string())
+            },
+        };
+    }
+
+    Err("not found".to_owned())
+}
+
+
+pub async fn get_host_addresses_for_single_rr_type<IPTYPE>(
+        client: &Client,
+        host_name: &String,
+        route53_zone_id: &String,
+        rrtype: RrType,
+) -> Result<Vec::<IPTYPE>, String>
+    where IPTYPE: std::str::FromStr,
+          <IPTYPE as std::str::FromStr>::Err: std::fmt::Display
+{
+    let mut result = Vec::<IPTYPE>::new();
+
+    let output = client.list_resource_record_sets()
+        .set_hosted_zone_id(Some(route53_zone_id.to_owned()))
+        .set_start_record_name(Some(host_name.to_owned()))
+        .set_start_record_type(Some(rrtype.clone()))
+        .set_max_items(Some(1))
+        .send()
+        .await
+    ;
+    match output {
+        Ok(output) => {
+            for rrs in output.resource_record_sets.iter() {
+                if &rrs.name != host_name || rrs.r#type != rrtype {
+                    break;
+                }
+
+                match &rrs.resource_records {
+                    Some(values) => {
+                        for value_struct in values.iter() {
+                            match value_struct.value.parse::<IPTYPE>() {
+                                Ok(ip) => { result.push(ip) },
+                                Err(e) => {
+                                    return Err(
+                                        format!("Got bad 'A' record value back from route53: \"{}\": {}", value_struct.value, e.to_string())
+                                    )
+                                }
+                            }
+                        }
+                    },
+                    None => {}
+                }
+            }
+        },
+        Err(e) => { return Err(e.to_string()) }
+    };
+
+    Ok(result)
+}
+
+
+pub async fn get_host_addresses(
+        client: &Client,
+        host_name: &String,
+        route53_zone_id: &Option<String>,
+) -> Result<Addresses, String> {
+    let zone_id = match route53_zone_id {
+        Some(value) => value.to_owned(),
+        None => get_zone_id(client, host_name).await?
+    };
+
+    let fut_ipv4 = get_host_addresses_for_single_rr_type::<Ipv4Addr>(
+        client, host_name, &zone_id, RrType::A
+    );
+    let fut_ipv6 = get_host_addresses_for_single_rr_type::<Ipv6Addr>(
+        client, host_name, &zone_id, RrType::Aaaa
+    );
+
+    let (result_ipv4, result_ipv6) = join!(fut_ipv4, fut_ipv6);
+    let result_ipv4 = match result_ipv4 {
+        Ok(rrs) => rrs,
+        Err(e) => { return Err(e.to_string()) }
+    };
+    let result_ipv6 = match result_ipv6 {
+        Ok(rrs) => rrs,
+        Err(e) => { return Err(e.to_string()) }
+    };
+
+    Ok(Addresses { v4: result_ipv4, v6: result_ipv6 })
+}
+
+
+trait IpAny : std::fmt::Display{}
+impl IpAny for Ipv4Addr {}
+impl IpAny for Ipv6Addr {}
+
+
+fn _build_r53_change_set(
+        host_name: String,
+        addresses: &Vec::<impl IpAny + Sized>,
+        rr_type: aws_sdk_route53::types::RrType,
+        action: aws_sdk_route53::types::ChangeAction
+) -> Result<aws_sdk_route53::types::Change, String> {
+    let mut rr_vec = Vec::<ResourceRecord>::new();
+    for ip in addresses.iter() {
+        let rr = match
+            aws_sdk_route53::types::ResourceRecord::builder()
+            .set_value(Some(ip.to_string()))
+            .build()
+        {
+            Ok(r) => r,
+            Err(e) => { return Err(e.to_string()) }
+        };
+        rr_vec.push(rr);
+    }
+
+    let rrs = match
+        aws_sdk_route53::types::ResourceRecordSet::builder()
+        .set_name(Some(host_name))
+        .set_type(Some(rr_type))
+        .set_resource_records(Some(rr_vec))
+        .build()
+    {
+        Ok(rrs) => rrs,
+        Err(e) => { return Err(e.to_string()) }
+    };
+    let chg = match
+        aws_sdk_route53::types::Change::builder()
+        .set_action(Some(action))
+        .set_resource_record_set(Some(rrs))
+        .build()
+    {
+        Ok(chg) => chg,
+        Err(e) => { return Err(e.to_string()) }
+    };
+
+    Ok(chg)
+}
+
+
+pub async fn set_host_addresses(
+        config: &Config,
+        desired_addresses: &Addresses,
+        current_addresses: &Addresses
+) -> Result<(), String> {
+    let mut changes = Vec::<aws_sdk_route53::types::Change>::new();
+ 
+    if desired_addresses.v4 != current_addresses.v4 {
+        if desired_addresses.v4.is_empty() {
+            changes.push(
+                _build_r53_change_set(
+                    config.host_name.to_owned(), 
+                    &current_addresses.v4, 
+                    aws_sdk_route53::types::RrType::A, 
+                    aws_sdk_route53::types::ChangeAction::Delete
+                )?
+            );
+        } else {
+            changes.push(
+                _build_r53_change_set(
+                    config.host_name.to_owned(), 
+                    &desired_addresses.v4, 
+                    aws_sdk_route53::types::RrType::A, 
+                    aws_sdk_route53::types::ChangeAction::Upsert
+                )?
+            );
+        }
+    }
+ 
+    if desired_addresses.v6 != current_addresses.v6 {
+        if desired_addresses.v6.is_empty() {
+            changes.push(
+                _build_r53_change_set(
+                    config.host_name.to_owned(), 
+                    &current_addresses.v6, 
+                    aws_sdk_route53::types::RrType::Aaaa, 
+                    aws_sdk_route53::types::ChangeAction::Delete
+                )?
+            );
+        } else {
+            changes.push(
+                _build_r53_change_set(
+                    config.host_name.to_owned(), 
+                    &desired_addresses.v6, 
+                    aws_sdk_route53::types::RrType::Aaaa,
+                    aws_sdk_route53::types::ChangeAction::Upsert
+                )?
+            );
+        }
+    }
+    
+    if changes.is_empty() {
+        return Ok(());
+    }
+    let start_time = Instant::now();
+    let expiry_time = start_time.checked_add(config.update_timeout.to_owned()).unwrap();
+
+    let cb = 
+        match
+            aws_sdk_route53::types::ChangeBatch::builder()
+            .set_changes(Some(changes))
+            .build()
+        {
+            Ok(cb) => cb,
+            Err(e) => { return Err(e.to_string()) }
+        }
+    ;
+    let change_fut = 
+        config.route53_client.change_resource_record_sets()
+        .set_change_batch(Some(cb))
+        .set_hosted_zone_id(config.route53_zone_id.to_owned())
+        .send()
+    ;
+    let timeout_fut = timeout(config.update_timeout.to_owned(), change_fut);
+
+    let timeout_output = match timeout_fut.await {
+        Ok(output) => output,
+        Err(_e) => { return Err("Timed out waiting for response".to_owned()) }
+    };
+    let change_output = match timeout_output {
+        Ok(output) => output,
+        Err(e) => { return Err(e.to_string()) }
+    };
+
+    let mut ci = change_output.change_info.unwrap();
+    loop {
+        if ci.status == aws_sdk_route53::types::ChangeStatus::Insync {
+            return Ok(());
+        }
+
+        let now = Instant::now();
+        let time_elapsed = now - start_time;
+        if time_elapsed <= config.update_timeout {
+            return Err("Timed out waiting for change to synchronize".to_owned());
+        }
+        let time_remaining = expiry_time - now;
+
+        sleep(std::cmp::min(time_remaining, config.update_poll_interval)).await;
+
+        let output =
+            match
+                config.route53_client.get_change()
+                .set_id(Some(ci.id))
+                .send()
+                .await
+            {
+                Ok(output) => output,
+                Err(e) => { return Err(e.to_string()) }
+            }
+        ;
+        ci = output.change_info.unwrap();
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 
 use std::sync::Arc;
 
-use log::{debug, error, trace};
+use log::{debug, error, trace, warn};
 
 mod addresses;
 mod aws_route53;
@@ -50,11 +50,44 @@ async fn main() {
         })
     };
 
+    let fut_r53_addresses = {
+        let arc_config = arc_config.clone();
+        set.spawn_local(async move {
+            let config = arc_config.as_ref();
+            crate::aws_route53::get_host_addresses(
+                &config.route53_client, &config.host_name, &config.route53_zone_id
+            ).await
+        })
+    };
+
     set.await;
-    let addresses = crate::addresses::Addresses { 
+
+    let addresses_current = crate::addresses::Addresses { 
         v4: fut_ipv4.await.unwrap(),
         v6: fut_ipv6.await.unwrap()
     };
-    debug!("Got {:?}", addresses);
+    debug!("Got current: {:?}", addresses_current);
+
+    let addresses_route53 = match fut_r53_addresses.await.unwrap() {
+        Ok(r) => r,
+        Err(e) => { error!("{}", e); return (); }
+    };
+    debug!("Got route53: {:?}", addresses_route53);
+
+    if addresses_current == addresses_route53 {
+        debug!("Current addresses match route53 configuration; no update required.");
+        return;
+    } else if args.no_update {
+        warn!("Current addresses DO NOT match configuration, but not updating due to --no_update");
+        std::process::exit(78);  // Code matches the BSD definition for "EX_CONFIG".
+    }
+
+    debug!("Address mismatch: attempting route53 update...");
+    match crate::aws_route53::set_host_addresses(
+        &*arc_config, &addresses_current, &addresses_route53, 
+    ).await {
+        Ok(()) => {debug!("Update successful")},
+        Err(e) => {error!("Update failed: {e}")}
+    };
 
 }
